dce2403398d54efddb5c6a0fff5527fee66b997a
Alexandra Seidel <khymeira@live.com>
2020-04-02 13:01:55 -0400

    removed uoe specific stuff, removed python stuff

--- coreconfig-install-macos10.14.sh
+++ coreconfig-install-macos10.14.sh
@@ -2,11 +2,12 @@
 
 ###################################################################
 #
-# Enable macOS re-install for Macs not on 10.14
+# Enable macOS install for Macs not on 10.14
 #
-# Date: Thu 20 Feb 2020 15:07:23 GMT
-# Version: 0.1.9
+# Date: Thu 2 Apr 2020 11:30:50 EST
+# Version: 0.2.1
 # Creator: ganders1
+# Updated By: khymeira for uva
 #
 ##################################################################
 
@@ -25,15 +26,11 @@ fi
 
 if ! [ -e "/Library/Application Support/JAMF/Receipts/Install_macOS_Mojave-14.6.06-1.sig.pkg" ]
 then
-	rm -fR "/Applications/Install macOS Mojave.app"
+	exit 0;
 fi
 
 osversion=`sw_vers -productVersion | awk -F . '{print $2}'`
 if [ $osversion == "14" ]; then
-    # Delete the login banner and receipt as OS is already on the one we want.
-	rm -fR /Library/Security/PolicyBanner.rtfd      
-    rm -dfR "/Library/Application Support/JAMF/Receipts/SavingPolicyBanner*.pkg"
-    
     # Do a recon
     /usr/local/bin/jamf recon
 fi
@@ -51,7 +48,7 @@ else
     exit 1;
 fi
 
-username=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+username=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 if [ -z $username ]; then
 	NoUser=True
@@ -108,19 +105,14 @@ rm -f /Users/Shared/Res
 exit 0;
 EOF
 
+
+#not sure if relevant to uva
 chmod +x "${resources}/postinstall"
 
 pkgbuild --nopayload --id ed.is.${pkg_name} --version ${version} --scripts "${resources}" "/Users/Shared/${pkg_name}-${version}.pkg"
 productbuild --identifier ed.is.${pkg_name}-${version} --package /Users/Shared/${pkg_name}-${version}.pkg /Users/Shared/dist-${pkg_name}-${version}.pkg
 
-if [ $macOS_app_vers -ge 145 ]; then
-
-    # delete the login banner as we are updating macOS
-	rm -fR /Library/Security/PolicyBanner.rtfd
-    
-    #For Labs, remove receipt for the login banner so it gets put back after upgrade.
-    rm -dfR "/Library/Application Support/JAMF/Receipts/SavingPolicyBanner*.pkg"
-    
+if [ $macOS_app_vers -ge 145 ]; then    
     if [ $NoUser == True ]; then
     	echo "No user present, starting osinstall"
 		/Applications/Install\ macOS\ Mojave.app/Contents/Resources/startosinstall --nointeraction --agreetolicense --installpackage /Users/Shared/dist-${pkg_name}-${version}.pkg
@@ -131,19 +123,10 @@ if [ $macOS_app_vers -ge 145 ]; then
     fi
     
 else
-
-	# Do a delete incase an older version is there
-	rm -fR "/Applications/Install macOS Mojave.app"
     
     # Add the installer
     /usr/local/bin/jamf policy -event OS-Installer-14
-    
-    # Delete the login banner as we are updating macOS
-	rm -fR /Library/Security/PolicyBanner.rtfd      
 
-    #For Labs, remove receipt for the login banner so it gets put back after upgrade.
-    rm -dfR "/Library/Application Support/JAMF/Receipts/SavingPolicyBanner*.pkg"
-    
     if [ $NoUser == True ]; then
     	echo "No user present, starting osinstall"
 		/Applications/Install\ macOS\ Mojave.app/Contents/Resources/startosinstall --nointeraction --agreetolicense --installpackage /Users/Shared/dist-${pkg_name}-${version}.pkg
